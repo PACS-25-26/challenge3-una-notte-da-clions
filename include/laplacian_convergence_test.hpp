3b8eeaee4c919a0c3388cdbd48f2d91983dd6cb9
Francesco Barrale <francesco.barrale@mail.polimi.it>
2026-05-26 18:26:31 +0200

    Completato file convergence_test.hpp

--- include/laplacian_convergence_test.hpp
+++ include/laplacian_convergence_test.hpp
@@ -25,6 +25,7 @@ namespace laplacian_solvers {
         std::vector<double> iterations_parallel;
         std::vector<double> times_serial;
         std::vector<double> times_parallel;
+        std::vector<double> speedups; // Parallel time / Serial time
     };
 
     template <SolverType solver_type, BoundaryCondition boundary_condition, typename funcType>
@@ -33,17 +34,21 @@ namespace laplacian_solvers {
         Data_Struct<funcType> data_original;
         std::vector<unsigned> refinement_levels = {16, 32, 64, 128, 256}; 
         double compute_error(const eigenMatrix& u_h, const eigenMatrix& X, const eigenMatrix& Y, funcType u_exact_lambda);
+        Convergence_Struct convergence_data_struct;
+        bool is_test_done = false;
+
         public:
         Convergence_Test(const Data_Struct<funcType>& data) : data_original(data) {}
         Convergence_Struct run_convergence_test();
+        void print_convergence_results() const;
+
     };
 
     template <SolverType solver_type, BoundaryCondition boundary_condition, typename funcType>
     Convergence_Struct Convergence_Test<solver_type, boundary_condition, funcType>::run_convergence_test() {
-        Convergence_Struct convergence_data;
 
         for(unsigned n_val : refinement_levels) {
-            convergence_data.n.push_back(n_val);
+            convergence_data_struct.n.push_back(n_val);
             
             data_original.n = n_val; 
 
@@ -57,11 +62,11 @@ namespace laplacian_solvers {
             auto end_serial = std::chrono::steady_clock::now();
 
             std::chrono::duration<double, std::milli> elapsed_serial = end_serial - start_serial;
-            convergence_data.times_serial.push_back(elapsed_serial.count());
+            convergence_data_struct.times_serial.push_back(elapsed_serial.count());
         
             double error_serial = compute_error(result_serial.u_h, result_serial.X, result_serial.Y, data_original.u_exact_lambda);
-            convergence_data.errors_serial.push_back(error_serial);
-            convergence_data.iterations_serial.push_back(result_serial.iterations);
+            convergence_data_struct.errors_serial.push_back(error_serial);
+            convergence_data_struct.iterations_serial.push_back(result_serial.iterations);
 
             // Parallel Run
             auto start_parallel = std::chrono::steady_clock::now();
@@ -73,15 +78,16 @@ namespace laplacian_solvers {
             auto end_parallel = std::chrono::steady_clock::now();
 
             std::chrono::duration<double, std::milli> elapsed_parallel = end_parallel - start_parallel;
-            convergence_data.times_parallel.push_back(elapsed_parallel.count());
+            convergence_data_struct.times_parallel.push_back(elapsed_parallel.count());
 
             double error_parallel = compute_error(result_parallel.u_h, result_parallel.X, result_parallel.Y, data_original.u_exact_lambda);
-            convergence_data.errors_parallel.push_back(error_parallel);
-            convergence_data.iterations_parallel.push_back(result_parallel.iterations);
-            
+            convergence_data_struct.errors_parallel.push_back(error_parallel);
+            convergence_data_struct.iterations_parallel.push_back(result_parallel.iterations);
+            convergence_data_struct.speedups.push_back(convergence_data_struct.times_serial[i] / convergence_data_struct.times_parallel[i]);
         }
 
-        return convergence_data;
+        is_test_done = true;
+        return convergence_data_struct;
     }
 
     template <SolverType solver_type, BoundaryCondition boundary_condition, typename funcType>
@@ -97,6 +103,26 @@ namespace laplacian_solvers {
         }
         return std::sqrt(error_sum);
 
+    }
+
+    template <SolverType solver_type, BoundaryCondition boundary_condition, typename funcType>
+    void Convergence_Test<solver_type, boundary_condition, funcType>::print_convergence_results() const {
+        if (!is_test_done) {
+            std::cerr << "Convergence test has not been run yet. Please run run_convergence_test() before printing results." << std::endl;
+            return;
+        } 
+        std::cout << "Refinement Level (n) | L2 Error (Serial) | L2 Error (Parallel) | Iterations (Serial) | Iterations (Parallel) | Time (ms, Serial) | Time (ms, Parallel) | Speedup" << std::endl;
+        for (size_t i = 0; i < convergence_data_struct.n.size(); ++i) {
+            std::cout << convergence_data_struct.n[i] << " | "
+                      << convergence_data_struct.errors_serial[i] << " | "
+                      << convergence_data_struct.errors_parallel[i] << " | "
+                      << convergence_data_struct.iterations_serial[i] << " | "
+                      << convergence_data_struct.iterations_parallel[i] << " | "
+                      << convergence_data_struct.times_serial[i] << " | "
+                      << convergence_data_struct.times_parallel[i] << " | "
+                      << convergence_data_struct.speedups[i] << std::endl;    
+        }
+        return;
     }
 
 }
